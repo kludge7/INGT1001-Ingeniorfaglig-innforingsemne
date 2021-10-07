f9058c47416a6b6f8f5197e9d91747fa26b8f296
AndrKluge <89779148+AndrKluge@users.noreply.github.com>
2021-10-08 00:08:28 +0200

    Create Lego Øving 1.py
    Lad opp batteriet på EV3-en din.
    2. Dersom du ikke har gjort det må du sette opp utviklingsmiljø ved å følge
    innstallasjonsguiden HER. Sjekk at du klarer å kompilere og kjøre programmet.
    3. Bygg en enkel Lego-bil med to motorer – en på hver side. Se “Tips og
    kode-eksempler” for oppskrift på en enkel og stødig robot.
    4. Lag et Python-program for roboten. Programmet skal gjøre følgende:
    a. Ved start skal teksten “Hello World” vises på LCD-skjermen.
    b. Deretter skal robotbilen kjøre i et rektangulært mønster, og stoppe etter å
    ha kjørt én runde.
    c. Til slutt skal roboten si “Have a nice day” før den skrur seg av.

---
+++ Lego Øving 1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=145)
+
+# Print message to screen
+ev3.screen.print("Hello World")
+wait(1000)
+
+# Drive in a square pattern
+for i in range(4):
+    robot.straight(750)
+    robot.turn(90)
+    ev3.speaker.beep()
+
+# Turn off car 
+ev3.speaker.say("Have a nice day")
+wait(1000)
